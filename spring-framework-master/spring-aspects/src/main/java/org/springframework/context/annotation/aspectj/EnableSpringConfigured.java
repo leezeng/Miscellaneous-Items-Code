d6bd071194c76be47bc135aecc0fefeedcc7f18b
Simba-cheng <simba_cheng@163.com>
2018-10-18 19:50:32 +0800

    Spring 源码 以及本地debug

--- spring-framework-master/spring-aspects/src/main/java/org/springframework/context/annotation/aspectj/EnableSpringConfigured.java
+++ spring-framework-master/spring-aspects/src/main/java/org/springframework/context/annotation/aspectj/EnableSpringConfigured.java
@@ -16,13 +16,13 @@
 
 package org.springframework.context.annotation.aspectj;
 
+import org.springframework.context.annotation.Import;
+
 import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
-
-import org.springframework.context.annotation.Import;
 
 /**
  * Signals the current application context to apply dependency injection to
