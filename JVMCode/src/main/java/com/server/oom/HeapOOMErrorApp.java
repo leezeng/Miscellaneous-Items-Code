c870a9e0b06b1952a2aef178f6d3be803f78c8e7
坚定不移得 <simba_cheng@163.com>
2018-10-25 10:36:38 +0800

    Update HeapOOMErrorApp.java
    update file

--- JVMCode/src/main/java/com/server/oom/HeapOOMErrorApp.java
+++ JVMCode/src/main/java/com/server/oom/HeapOOMErrorApp.java
@@ -14,11 +14,14 @@ public class HeapOOMErrorApp {
         testHeapOOM();
 
     }
-
+    
     /**
      * 测试-堆溢出
-     *
+     * <p>
      * VM Args : -Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+     * <p>
+     * 首先限定堆内存的大小(最大和最小都限制为20m)
+     * 在堆中创建一个List，存储HeapOOMObject。while(true)一直向其中塞入对象，直到其内存溢出
      */
     public static void testHeapOOM() {
 
