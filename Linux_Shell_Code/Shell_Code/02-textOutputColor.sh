5768dbee9651b0651e810910757dc6268d239945
坚定不移得 <simba_cheng@163.com>
2018-10-29 21:35:36 +0800

    02-textOutputColor.sh
    create 02-textOutputColor.sh

---
+++ Linux_Shell_Code/Shell_Code/02-textOutputColor.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+######文字颜色######
+#重置=0，黑色=30，红色=31，绿色=32，黄色=33，蓝色=34，洋红=35，青色=36，白色=37
+
+#红色
+echo -e "\e[1;31m This is red text \e[0m";
+
+#绿色
+echo -e "\e[1;32m This is red text \e[0m";
+
+#黄色
+echo -e "\e[1;33m This is red text \e[0m";
+
+#蓝色
+echo -e "\e[1;34m This is red text \e[0m";
+
+#洋红
+echo -e "\e[1;35m This is red text \e[0m";
+
+#青色
+echo -e "\e[1;36m This is red text \e[0m";
+
+#白色
+echo -e "\e[1;37m This is red text \e[0m";
+
+
+echo "";
+echo "";
+
+
+######背景颜色######
+#重置=0，黑色=40，红色=41，绿色=42，黄色=43，蓝色=44，洋红=45，青色=46，白色=47
+
+#绿色
+echo -e "\e[1;42m Green Background \e[0m";
+
+#红色
+echo -e "\e[1;41m Green Background \e[0m";
+
+#黄色
+echo -e "\e[1;43m Green Background \e[0m";
+
+#蓝色
+echo -e "\e[1;44m Green Background \e[0m";
+
+#洋红
+echo -e "\e[1;45m Green Background \e[0m";
+
+#青色
+echo -e "\e[1;46m Green Background \e[0m";
+
+#白色
+echo -e "\e[1;47m Green Background \e[0m";
