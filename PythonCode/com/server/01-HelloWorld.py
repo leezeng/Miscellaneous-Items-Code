05a8a7a775c765c6446e8491153d76a28e9b5e73
Simba-cheng <simba_cheng@163.com>
2018-11-02 07:10:55 +0800

    Python Code

---
+++ PythonCode/com/server/01-HelloWorld.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+print('Hello World')
+
+print("Hello World")
+
+print("Hello ", "World ", "Python")
+
+print(100 + 200)
+
+print("100+200 =", 100 + 200)
+
+# 控制台输入参数,再打印
+name = input()
+print("name : ", name)
